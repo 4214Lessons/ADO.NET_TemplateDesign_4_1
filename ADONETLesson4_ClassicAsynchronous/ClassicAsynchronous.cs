7bbc1926dcdf66e7976c52cf5504a35239b91477
Tural Novruzov <turalinovruzov@gmail.com>
2023-01-19 13:40:25 +0400

    Add project files.

---
+++ ADONETLesson4_ClassicAsynchronous/ClassicAsynchronous.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+
+
+namespace ADONETLesson4_ClassicAsynchronous;
+
+
+public partial class ClassicAsynchronous : Form
+{
+    IConfigurationRoot? configurationRoot = null;
+    string conStr = string.Empty;
+
+
+    public ClassicAsynchronous()
+    {
+        InitializeComponent();
+        Configure();
+    }
+
+
+    private void Configure()
+    {
+        string projectPath = Directory.GetCurrentDirectory().Split(@"bin\")[0];
+       
+        configurationRoot = new ConfigurationBuilder()
+            .SetBasePath(projectPath)
+            .AddJsonFile("appsettings.json").Build();
+
+
+
+        conStr = configurationRoot.GetConnectionString("ConStr");
+    }
+}
